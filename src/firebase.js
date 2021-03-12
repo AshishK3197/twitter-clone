2912b8f1f6b6b88f06d8344e4be30e236bf89e04
kumarash <ashish.kumar@autodesk.com>
2021-03-13 00:53:08 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCollld1v7ybcuO1pqilt93BSU7KSp4UKo",
+	authDomain: "twitter-clone-bf100.firebaseapp.com",
+	projectId: "twitter-clone-bf100",
+	storageBucket: "twitter-clone-bf100.appspot.com",
+	messagingSenderId: "1084877853131",
+	appId: "1:1084877853131:web:c4d6bdf7ff22f01ef1664e",
+};
