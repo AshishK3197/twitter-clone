df7a9378ae132ad22c9c7e879b011e37fef0db0e
kumarash <ashish.kumar@autodesk.com>
2021-03-13 02:30:34 +0530

    Implemented the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Sidebar from "./Components/Sidebar";
 
 function App() {
 	return (
 		<div className="app">
 			<h1>Twitter-clone</h1>
+
+			{/* Sidebar */}
+			<Sidebar />
+			{/* Feed */}
+
+			{/* Widget */}
 		</div>
 	);
 }
---
+++ src/Components/SidebarOption.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOption({ active, text, Icon }) {
+	// console.log(active);
+	return (
+		<div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+			<Icon />
+			<h2>{text}</h2>
+		</div>
+	);
+}
+
+export default SidebarOption;
