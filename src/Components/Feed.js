8d06bf1de7b087d01333167a4821491e0a793549
kumarash <ashish.kumar@autodesk.com>
2021-03-14 02:17:34 +0530

    Added functionality to sync with firebase firestore

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import db from "../firebase";
 import "./Feed.css";
 import Post from "./Post";
 import Tweetbox from "./Tweetbox";
 
 function Feed() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		db.collection("posts").onSnapshot((snapshot) => {
+			setPosts(snapshot.docs.map((doc) => doc.data()));
+		});
+	}, []);
+
 	return (
 		<div className="feed">
 			{/* Header */}
@@ -14,14 +23,18 @@ function Feed() {
 			{/* Tweetbox */}
 			<Tweetbox />
 			{/* Post */}
-			<Post
-				displayName="Ashish Kumar"
-				username="AshishK3197"
-				verified={true}
-				text="Yoo Its working"
-				avatar="https://yt3.ggpht.com/yti/ANoDKi5vKosgmZ6ypE2umitiPIftiZWdAs5nroMP0U6kEg=s88-c-k-c0x00ffffff-no-rj-mo"
-				image="https://media.giphy.com/media/WU6cG51Jq1DNP2gD70/giphy.gif"
-			/>
+			{posts.map((post) => {
+				return (
+					<Post
+						displayName={post.displayName}
+						username={post.username}
+						verified={post.verified}
+						text={post.text}
+						avatar={post.avatar}
+						image={post.image}
+					/>
+				);
+			})}
 		</div>
 	);
 }
--- src/Components/Tweetbox.js
+++ src/Components/Tweetbox.js
@@ -1,21 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tweetbox.css";
 import { Avatar, Button } from "@material-ui/core";
+import db from "../firebase";
 
 function Tweetbox() {
+	const [tweetMessage, setTweetMessage] = useState("");
+	const [tweetImage, setTweetImage] = useState("");
+
+	const sendTweet = (e) => {
+		e.preventDefault();
+		db.collection("posts").add({
+			displayName: "Ashish Kumar",
+			username: "AshishK3197",
+			verified: true,
+			text: tweetMessage,
+			image: tweetImage,
+			avatar:
+				"https://yt3.ggpht.com/yti/ANoDKi5vKosgmZ6ypE2umitiPIftiZWdAs5nroMP0U6kEg=s88-c-k-c0x00ffffff-no-rj-mo",
+		});
+		setTweetMessage("");
+		setTweetImage("");
+	};
 	return (
 		<div className="tweetbox">
 			<form action="">
 				<div className="tweetbox__input">
 					<Avatar src="https://yt3.ggpht.com/yti/ANoDKi5vKosgmZ6ypE2umitiPIftiZWdAs5nroMP0U6kEg=s88-c-k-c0x00ffffff-no-rj-mo" />
-					<input placeholder="What's Happening" type="text" />
+					<input
+						value={tweetMessage}
+						placeholder="What's Happening"
+						type="text"
+						onChange={(e) => setTweetMessage(e.target.value)}
+					/>
 				</div>
 				<input
 					className="tweetbox__imageInput"
+					value={tweetImage}
 					placeholder="Enter image URL"
 					type="text"
+					onChange={(e) => {
+						setTweetImage(e.target.value);
+					}}
 				/>
-				<Button className="tweetbox__tweeetbutton">Tweet</Button>
+				<Button
+					onClick={sendTweet}
+					type="submit"
+					className="tweetbox__tweeetbutton"
+				>
+					Tweet
+				</Button>
 			</form>
 		</div>
 	);
